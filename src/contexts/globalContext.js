d1946b5ab18c9dbf88f66cef8a3bfe1ff54aa8af
FrancescoPalazzo97 <francescopalazzo97@gmail.com>
2025-06-16 12:44:03 +0200

    [FEAT] Add GlobalContext

--- src/contexts/globalContext.js
+++ src/contexts/globalContext.js
@@ -1,1 +1,17 @@
-import axios from 'axios';
+import { createContext, useState, useEffect, useContext, Children } from "react";
+
+const GlobalContext = createContext();
+
+const GlobalProvider = ({ childre }) => {
+
+    return <GlobalContext.Provider value={{}}>
+        {Children}
+    </GlobalContext.Provider>
+}
+
+const useGlobalContext = () => {
+    const context = useContext(GlobalContext);
+    return context;
+}
+
+export { GlobalProvider, useGlobalContext };
