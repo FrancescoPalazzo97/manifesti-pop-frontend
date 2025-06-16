997d6a076d426e846c8d0fdf6f6c168824bb4215
FrancescoPalazzo97 <francescopalazzo97@gmail.com>
2025-06-16 12:56:09 +0200

    Fixing GlobalContext

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -6,7 +6,6 @@ const HomePage = () => {
         <div className='col-12'>
             <h1>I poster più Belli che ci siano</h1>
             <div className="row gy-4">
-                <p>a</p>
                 <PosterCard />
             </div>
         </div>
