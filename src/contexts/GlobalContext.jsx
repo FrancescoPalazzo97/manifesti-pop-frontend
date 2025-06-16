f62e638a9835770bf3b7276daa6e1f3f488885b0
Ilaria <maffiolailaria1@gmail.com>
2025-06-16 13:24:16 +0200

    [REF] Remove posters state and data fetching from GlobalContext

--- src/contexts/GlobalContext.jsx
+++ src/contexts/GlobalContext.jsx
@@ -4,24 +4,7 @@ import axios from "axios";
 const GlobalContext = createContext();
 
 const GlobalProvider = ({ children }) => {
-  const [posters, setPosters] = useState([]);
-
-  useEffect(() => {
-    axios
-      .get("http://127.0.0.1:3000/posters/")
-      .then((res) => {
-        setPosters(res.data);
-      })
-      .catch((err) => {
-        console.log("Errore caricamento poster:", err);
-      });
-  }, []);
-
-  return (
-    <GlobalContext.Provider value={{ posters }}>
-      {children}
-    </GlobalContext.Provider>
-  );
+  return <GlobalContext.Provider value={{}}>{children}</GlobalContext.Provider>;
 };
 
 const useGlobalContext = () => {
