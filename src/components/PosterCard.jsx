25913af31f063ac9ceb726829dac2455ad68c000
Tibaldi-Nicol <nickkoltibaldi@gmail.com>
2025-06-17 11:52:59 +0200

    [UPD]Final-Product-Detail-funzionante

--- src/components/PosterCard.jsx
+++ src/components/PosterCard.jsx
@@ -1,38 +1,29 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-const PosterCard = ({ id, title, artist, size, price, image }) => {
+const PosterCard = ({ title, artist, size, price, image, slug }) => {
+  const newSize = () => {
+    if (size === 'sm') return 'Small';
+    if (size === 'md') return 'Medium';
+    if (size === 'lg') return 'Large';
+    return 'Sconosciuta';
+  };
 
-    const newSize = () => {
-        if (size === 'sm') {
-            return 'Small'
-        }
-        if (size === 'md') {
-            return 'Medium'
-        }
-        else (size === 'lg')
-        return 'Large'
-    }
+  return (
+    <Link to={`/posters/${slug}`} className='text-decoration-none'>
+      <div className="card h-100 w-100 d-flex flex-column">
+        <div className="img-container">
+          <img src={image} className="fix-img" alt={title} />
+        </div>
+        <div className="card-body text-center flex-grow-1 d-flex flex-column justify-content-between">
+          <h5 className="card-title fw-bold">{title}</h5>
+          <p className="card-text text-muted">{artist}</p>
+          <p className="card-text text-muted">Taglia: {newSize()}</p>
+          <p className="fw-bold"><strong>{price}€</strong></p>
+        </div>
+      </div>
+    </Link>
+  );
+};
 
-    return (
-        <>
-            < Link to={`/product/${id}`} className='text-decoration-none'>
-                <div className="card h-100 w-100 d-flex flex-column">
-                    <div className="img-container">
-                        <img src={image} className="fix-img" alt={title} />
-                    </div>
-                    <div className="card-body text-center flex-grow-1 d-flex flex-column justify-content-between">
-                        <h5 className="card-title fw-bold">{title}</h5>
-                        <p className="card-text text-muted">{artist}</p>
-                        <p className="card-text text-muted">Taglia: {newSize()}</p>
-                        <p className="fw-bold"><strong>{price}€</strong></p>
-                    </div>
-                </div>
-            </Link >
-        </>
-
-    )
-}
-
-export default PosterCard
-
+export default PosterCard;
