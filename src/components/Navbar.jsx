cb9adf325f8794293967c2ace65953a108250cb0
FrancescoPalazzo97 <francescopalazzo97@gmail.com>
2025-06-17 15:06:21 +0200

    [FIX] Fix navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,14 @@
-import React from "react";
 import "./navbar.css";
 import { NavLink } from "react-router-dom";
+//import { Navbar, Nav, Container, Form, Button, InputGroup } from 'react-bootstrap';
+import { Navbar, Nav, Container, Form, FormControl, Button } from "react-bootstrap";
 
 const navbar = () => {
+
   return (
     <>
-      <nav className="d-flex justify-content-between align-items-center p-2 navbar-bg-color">
+      {/* ------------------- LASCIO QUELLO PRECEDENTE ---------------------------------------------------*/}
+      {/* <nav className="d-flex justify-content-between align-items-center p-2 navbar-bg-color">
         <NavLink to="/" className="text-decoration-none text-white">
           <div className="d-flex align-items-center">
             <img src="/logo-header.png" alt="" className="img-size" />
@@ -37,7 +40,43 @@ const navbar = () => {
             </button>
           </form>
         </div>
-      </nav>
+      </nav> */}
+
+      <Navbar expand="lg" className="navbar-bg-color">
+        <Container fluid>
+          <NavLink to="/" className="text-decoration-none text-white">
+            <div className="d-flex align-items-center">
+              <img src="/logo-header.png" alt="" className="img-size" />
+              <div className="ms-3 title-style">Manifesti Pop</div>
+            </div>
+          </NavLink>
+
+          {/* Bottone toggle per dispositivi mobili */}
+          <Navbar.Toggle aria-controls="navbarTogglerDemo02" />
+
+          {/* Menu collassabile */}
+          <Navbar.Collapse id="navbarTogglerDemo02">
+            <Nav className="me-auto mb-2 mb-lg-0 ms-lg-5">
+              <NavLink to="" className="text-light nav-link">Artisti</NavLink>
+              <NavLink to="" className="text-light nav-link">Whishlist</NavLink>
+              <NavLink to="" className="me-2 nav-link">
+                <i className="fa-solid fa-cart-shopping text-white"></i>
+              </NavLink>
+            </Nav>
+
+            {/* Form di ricerca */}
+            <Form className="d-flex" role="search">
+              <FormControl
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+              />
+              <Button className="btn btn-dark" type="submit">Search</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </>
   );
 };
