c3c7e7d840f6b334299839ae8504b05c31bb55a2
Ilaria <maffiolailaria1@gmail.com>
2025-06-25 11:31:54 +0200

    [UPD] improved styling consistency in FreeShipment component

--- src/components/FreeShipment.jsx
+++ src/components/FreeShipment.jsx
@@ -1,13 +1,13 @@
 const FreeShipment = () => {
-    return (
-        <>
-            <div className="freeshipment col 12">
-                <h4 className='text-center p-2 m-0'>
-                    Spedizione Gratuita per ordini sopra i 100€!✈️
-                </h4>
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <div className="freeshipment col 12">
+        <h4 className="text-center p-2 m-0 fs-5">
+          Spedizione Gratuita per ordini sopra i 100€!✈️
+        </h4>
+      </div>
+    </>
+  );
+};
 
-export default FreeShipment
+export default FreeShipment;
